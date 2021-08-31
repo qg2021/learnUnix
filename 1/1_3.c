351d2ff1b3d2d1b986d70a745bf7012a201385f6
qg <1766964745@qq.com>
2021-09-01 01:44:56 +0800

    1_3:read directory code
    qg_2
    
    Signed-off-by: qg <1766964745@qq.com>

---
+++ 1/1_3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <dirent.h>
+
+int main(int argc, char *argv[])
+{
+	DIR *dp;
+	struct dirent *dirp;
+	
+	if(argc != 2){
+		printf("usage: ls directory_name\n");
+		return -1;
+	}
+	
+	if((dp = opendir(argv[1])) == NULL){
+		printf("can't open directory %s\n",argv[1]);
+		return -1;
+	}
+
+	while((dirp = readdir(dp)) != NULL){
+		printf("%s\n",dirp->d_name);
+	}
+	
+	return 1;
+}
